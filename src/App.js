d4d86e33ea141ec2f02d8c1708f89b6508eb87dc
jitender singh <jeetusinghkanyal@gmail.com>
2022-11-24 23:20:29 +0530

    adding different component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@
 import './App.css';
 import Navbar from './component/Navbar';
 import Hero from './component/Hero';
+import Business from './component/Business';
+import Billing from './component/Billing';
+import Deal from './component/Deal';
+import People from './component/People';
 
 function App() {
   return (
     <>
      <Navbar />
      <Hero />
+     <Business />
+     <Billing/>
+     <Deal />
+     <People />
     </>
   );
 }
